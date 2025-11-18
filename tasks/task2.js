cc4bc4aef91a549a1a0c4c8e7c30f40dfe2a1c13
Wu1v2z <artyr.comfy3065@gmail.com>
2025-11-18 21:53:09 +0200

    Update task2.js

--- tasks/task2.js
+++ tasks/task2.js
@@ -12,13 +12,12 @@ const products = [
 ];
 
 function getProductsNotInPriceRange(products) {
-  // 1. Фільтруємо: залишаємо товари з ціною < 100 АБО > 500
+  
   const highLowProducts = products.filter(product => {
     return product.price < 100 || product.price > 500;
   });
 
-  // 2. Трансформуємо (map): перетворюємо масив об'єктів на масив рядків (імен товарів)
-  // Це виправляє помилку тесту, який очікує лише імена.
+
   const productNames = highLowProducts.map(product => product.name);
 
   return productNames;
